c4c77d45faac7413db25bd942f6cb9d24d70d841
Saad Masood <me@itssaad.com>
2015-05-17 04:41:55 +0500

    finally migration created

--- lib/con_sync/version.rb
+++ lib/con_sync/version.rb
@@ -1,3 +1,3 @@
 module ConSync
-  VERSION = "0.0.6"
+  VERSION = "0.0.7"
 end
--- lib/generators/install_con_sync/install_con_sync_generator.rb
+++ lib/generators/install_con_sync/install_con_sync_generator.rb
@@ -2,10 +2,14 @@ require 'rails/generators/active_record'
 
 class InstallConSyncGenerator < Rails::Generators::Base
   include Rails::Generators::Migration
-  extend ActiveRecord::Generators::Migration
+
+  def self.next_migration_number(path)
+    ActiveRecord::Generators::Base.next_migration_number(path)
+  end
+
   source_root File.expand_path('../templates', __FILE__)
 
   def  generate_migration
-    migration_template "create_con_sync_schema", "db/migrate/create_con_sync.rb"
+    migration_template "create_con_sync_schema.rb", "db/migrate/create_con_sync.rb"
   end
 end
