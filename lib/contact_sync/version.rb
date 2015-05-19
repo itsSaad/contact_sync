7bf72824c8a27b9596866b13db7694e1456ef8eb
Saad Masood <me@itssaad.com>
2015-05-19 19:12:46 +0500

    bug fix

--- lib/contact_sync/version.rb
+++ lib/contact_sync/version.rb
@@ -1,3 +1,3 @@
 module ContactSync
-  VERSION = "0.4.3"
+  VERSION = "0.4.4"
 end
--- lib/generators/install_contact_sync/templates/email.rb
+++ lib/generators/install_contact_sync/templates/email.rb
@@ -2,4 +2,13 @@ class Email < ActiveRecord::Base
 
   belongs_to :contact
 
+  def email
+    return self.encrypted_email.decrypt(:symmetric)
+  end
+
+  def email=(string)
+    self.encrypted_email = string.encrypt(:symmetric)
+    self.encrypted_email
+  end
+
 end
