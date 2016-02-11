ee63f6100fc4683b18d791a2a12d08cf6d7bdaca
Saad Masood <itssaad@icloud.com>
2016-02-11 13:11:10 +0500

    rspec gem updated. travis included.
    rspec gem updated. travis included.

--- contact_sync.gemspec
+++ contact_sync.gemspec
@@ -23,5 +23,5 @@ Gem::Specification.new do |s|
   s.add_dependency 'pry-rails'
 
   s.add_development_dependency "sqlite3"
-  s.add_development_dependency "rspec"
+  s.add_development_dependency "rspec", "~> 3.4"
 end
--- lib/contact_sync/syncable.rb
+++ lib/contact_sync/syncable.rb
@@ -71,11 +71,11 @@ module ContactSync
             #lets create phone_numbers that have been added in this contact.
             phones = con[:phones]
             phones.each do |ph|
-              binding.pry
               found_phone = theContact.phones.where(encrypted_number: ph[:number].extract_country_code[-1].encrypt(:symmetric)).limit(1).first
-              binding.pry
               if !found_phone
                 theContact.phones.create(phone_params(ph))
+              else
+                puts 'modified phone_number is already present. Ignoring situation.'
               end
             end
             if !theContact.update_attributes(contact_params(con))
